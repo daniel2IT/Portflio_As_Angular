bd6daa4f86fa566734788120f3a81fbfe56d1243
YOUR NAME <YOUR@EMAIL.com>
2020-11-08 01:21:51 +0200

    Final_Moddified

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  myimaage1:string="../../../assets/img/cv/Daniel_Vaskevic_CV.png";
+
   constructor() { }
 
   ngOnInit(): void {
